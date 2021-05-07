7d2696ee170034b7a64f7f268dd625d5f9ad7045
Ravivarman M <77786164+ravivarmanm@users.noreply.github.com>
2021-05-07 20:53:03 +0530

    Update script.js

--- WEBDL4WEEK/assignment 4/script.js
+++ WEBDL4WEEK/assignment 4/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+names[0]="ravi";
+names[1]="varman";
+names[2]="arul";
+names[3]="names";
+names[4]="messi";
+names[5]="suarez";
+names[6]="ironman";
+names[7]="tonystark";
+names[8]="america";
+names[9]="loparaas";
 
 
 for (var i = 0; i < names.length; i++) {
